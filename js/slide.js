61aeb51914d0f69f0df5b25f5e37964fe4283e72
Ronny Perez <ronnypdev@gmail.com>
2020-08-19 17:45:28 -0400

    Adding a box shadow on hover

--- js/slide.js
+++ js/slide.js
@@ -12,8 +12,37 @@ let z = 1;
 // when i lick the slide area, change the slide base on z-index
 slideArea.addEventListener('click', function () {
   currentSlide = currentSlide + 1;
+
+  if (currentSlide > images.length - 1) {
+    currentSlide = 0;
+  }
+
   z = z + 1;
+
+  // remove the animation from the style for every image
+  images.forEach((image) => {
+    image.style.animation = '';
+  });
 
   // pick the right image
   images[currentSlide].style.zIndex = z;
+  images[currentSlide].style.animation = 'fade 0.5s';
+});
+
+// When input my mouse over the slide area, put all of the images
+// in a random place
+slideArea.addEventListener('mouseover', () => {
+  images.forEach((image) => {
+    const x = 25 * Math.floor(Math.random() * 5) - 50;
+    const y = 25 * Math.floor(Math.random() * 5) - 50;
+
+    image.style.transform = `translate(${x}px, ${y}px)`;
+  });
+});
+
+// when i move my mouse away, put the images back
+slideArea.addEventListener('mouseout', () => {
+  images.forEach((image) => {
+    image.style.transform = '';
+  });
 });
